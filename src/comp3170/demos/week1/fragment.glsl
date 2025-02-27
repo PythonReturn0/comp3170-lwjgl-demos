a8873aef77f1e8f4a97076078a43bb650b4277db
Zihao <>
2025-02-27 22:09:53 +1100

    Update fragment.glsl

--- src/comp3170/demos/week1/fragment.glsl
+++ src/comp3170/demos/week1/fragment.glsl
@@ -7,7 +7,8 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 v = abs(p - vec2(0.5, 0.5));
+   float d = v.x + v.y;          // calculate distance to midpoint
 
    if (d < 0.5) {
       o_colour = vec4(u_colour, 1);
